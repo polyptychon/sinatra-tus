d1256503d49976e43ccd5796a4455dfddc5e0ff8
harris sidiropoulos <harris.sidiropoulos@gmail.com>
2014-07-04 13:34:22 +0300

    Handle patch to save file

--- app.rb
+++ app.rb
@@ -16,6 +16,7 @@ get "/files/" do
   haml :upload
 end
 
+# Handle OPTION-request (Check if we can upload files)
 options "/files/" do
   if request.env["Access-Control-Request-Method"]=="POST"
     response.headers["Content-Length"] = "0"
@@ -26,51 +27,31 @@ options "/files/" do
   end
 end
 
+# Handle POST-request (Create File)
 post "/files/" do
   response.headers["Location"] = "http://localhost:1080/files/24e533e02ec3bc40c387f1a0e460e216"
   status "201 Created"
 end
-post "/files/:filename" do
-  response.headers["Location"] = "http://localhost:1080/files/24e533e02ec3bc40c387f1a0e460e216"
-  status "200"
-end
-# Handle POST-request (Receive and save the uploaded file)
-post "/files" do
-  file_name = params['myfile'][:filename] || env['HTTP_X_FILENAME']
-  path = 'uploads/'+file_name
-  data_bytes = params['myfile'][:tempfile] || request.body
-  headers['Offset'] if request.env["Final-Length"]
-  File.open(path, "w") do |f|
-    f.write(data_bytes.read)
-  end
-  status 200
-end
 
-# Handle PATCH-request (Receive and save the uploaded file chunks)
-patch "/files/" do
-  file_name = params['myfile'][:filename] || env['HTTP_X_FILENAME']
-  path = 'uploads/'+file_name
-  data_bytes = params['myfile'][:tempfile] || request.body
-  offset = request.env['Offset']
-
-  File.open(path, "w") do |f|
-    f.seek(offset)
-    f.write(data_bytes.read)
-    headers['Offset'] = f.size.to_s
-  end
-  status 200
-end
-
-# Handle HEAD-request (Check if file exists)
-head "/files/" do
-  file_name = params['myfile'][:filename] || env['HTTP_X_FILENAME']
+# Handle OPTIONS-request (Check if file exists)
+options "/files/*.*" do |path, ext|
+  file_name = "#{path}.#{ext}"
   path = 'uploads/'+file_name
   if File.file?(path)
-    File.open(path, "r") do |f|
-      headers['Offset'] = f.size.to_s
-    end
     status 200
-  else
-    status 404
+  elsif
+    status 403
   end
+end
+# Handle PATCH-request (Receive and save the uploaded file)
+patch "/files/*.*" do |path, ext|
+  file_name = "#{path}.#{ext}"
+  path = 'uploads/'+file_name
+  data_bytes = params['myfile'][:tempfile] || request.body
+  File.open(path, "w") do |f|
+    f.write(data_bytes.read)
+  end
+  status 200
+  response.headers["Location"] = "http://localhost:1080/files/24e533e02ec3bc40c387f1a0e460e216"
+  status "200"
 end
