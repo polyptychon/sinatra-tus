1c762ebd3a740fa9416fafbdf9b268c10d0c5f13
harris sidiropoulos <harris.sidiropoulos@gmail.com>
2014-07-04 16:06:45 +0300

    if request FileName with path exists create path folders

--- app.rb
+++ app.rb
@@ -1,6 +1,7 @@
 require 'rubygems'
 require 'sinatra'
 require 'haml'
+require 'fileutils'
 require 'securerandom'
 require 'rickshaw'
 
@@ -8,18 +9,20 @@ before do
   response.headers["Access-Control-Allow-Methods"] = "HEAD,GET,PUT,POST,PATCH,DELETE"
   response.headers["Access-Control-Allow-Origin"] = "*"
   response.headers["Access-Control-Expose-Headers"] = "Location, Range, Content-Disposition, Offset"
-  response.headers["Access-Control-Allow-Headers"] = "Origin, X-Requested-With, Content-Type, Accept, Content-Disposition, Final-Length, Offset"
+  response.headers["Access-Control-Allow-Headers"] = "Origin, X-Requested-With, Content-Type, Accept, Content-Disposition, Final-Length, Offset, FileName"
   response.headers["Content-Type"] = "text/plain; charset=utf-8"
 end
 
 
 # Handle GET-request (Show the upload form)
-get "/files/*" do
-  return "hello world".to_sha1
-  #haml :upload
+# todo: add upload client
+get "/files/" do
+  #return "hello world".to_sha1
+  haml :upload
 end
 
 # Handle OPTION-request (Check if we can upload files)
+# todo: add security
 options "/files/" do
   if request.env["Access-Control-Request-Method"]=="POST"
     status 200
@@ -29,8 +32,18 @@ options "/files/" do
 end
 
 # Handle POST-request (Create File)
+# todo: check avaliable drive space
 post "/files/" do
-  unique_filename = SecureRandom.hex
+  file_size = request.env["Final-Length"]
+  request_file_name = request.env["FileName"]
+  if request_file_name
+    folders = request_file_name.split("/")
+    folders.pop
+    if folders.size>0
+      FileUtils.makedirs folders.join("/")
+    end
+  end
+  unique_filename = request_file_name || SecureRandom.hex
   path = 'uploads/'+unique_filename
   File.write(path, "")
   response.headers["Location"] = request.url+unique_filename
@@ -41,7 +54,6 @@ end
 options "/files/*" do
   file_name = params[:splat].join("").to_s
   path = 'uploads/'+file_name
-  response.headers["Test-Path"] = path.to_s
   if File.file?(path)
     status 200
   elsif
