6c2249c122ccd72e73288010e1cfa5b57c799c3a
harris sidiropoulos <harris.sidiropoulos@gmail.com>
2014-07-04 15:31:47 +0300

    Basic upload functionality finished

--- app.rb
+++ app.rb
@@ -1,6 +1,8 @@
 require 'rubygems'
 require 'sinatra'
 require 'haml'
+require 'securerandom'
+require 'rickshaw'
 
 before do
   response.headers["Access-Control-Allow-Methods"] = "HEAD,GET,PUT,POST,PATCH,DELETE"
@@ -13,45 +15,59 @@ end
 
 # Handle GET-request (Show the upload form)
 get "/files/" do
-  haml :upload
+  return "hello world".to_sha1
+  #haml :upload
 end
 
 # Handle OPTION-request (Check if we can upload files)
 options "/files/" do
   if request.env["Access-Control-Request-Method"]=="POST"
-    response.headers["Content-Length"] = "0"
     status 200
   elsif request.env["Access-Control-Request-Method"]=="PATCH"
-    response.headers["Content-Length"] = "0"
-    status 200
+    status 404
   end
 end
 
 # Handle POST-request (Create File)
 post "/files/" do
-  response.headers["Location"] = "http://localhost:1080/files/24e533e02ec3bc40c387f1a0e460e216"
-  status "201 Created"
+  unique_filename = SecureRandom.hex
+  path = 'uploads/'+unique_filename
+  File.write(path, "")
+  response.headers["Location"] = request.url+unique_filename
+  status 201
 end
 
 # Handle OPTIONS-request (Check if file exists)
-options "/files/*.*" do |path, ext|
-  file_name = "#{path}.#{ext}"
+options "/files/:filename" do
+  file_name = params["filename"]
   path = 'uploads/'+file_name
+  response.headers["Test-Path"] = path.to_s
   if File.file?(path)
     status 200
   elsif
-    status 403
+    status 404
   end
 end
+
 # Handle PATCH-request (Receive and save the uploaded file)
-patch "/files/*.*" do |path, ext|
-  file_name = "#{path}.#{ext}"
+patch "/files/:filename" do
+  file_name = params["filename"]
   path = 'uploads/'+file_name
-  data_bytes = params['myfile'][:tempfile] || request.body
+  data_bytes = request.body
   File.open(path, "w") do |f|
     f.write(data_bytes.read)
   end
   status 200
-  response.headers["Location"] = "http://localhost:1080/files/24e533e02ec3bc40c387f1a0e460e216"
-  status "200"
+end
+
+# Handle OPTIONS-request (Check if file exists)
+head "/files/:filename" do
+  file_name = params["filename"]
+  path = 'uploads/'+file_name
+  if File.file?(path)
+    response.headers["Offset"] = File.size(path).to_s
+    status 200
+  elsif
+    status 404
+  end
 end
