ac2676a41ed4c8a4bd649dca87d092a7b2533c90
harris sidiropoulos <harris.sidiropoulos@gmail.com>
2014-07-10 11:55:09 +0300

    clean up and md5 checksum

--- app.rb
+++ app.rb
@@ -6,19 +6,20 @@ require 'securerandom'
 require 'rickshaw'
 
 before do
-  response.headers["Access-Control-Allow-Methods"] = "HEAD,GET,PUT,POST,PATCH,DELETE"
-  response.headers["Access-Control-Allow-Origin"] = "*"
-  response.headers["Access-Control-Expose-Headers"] = "Location, Range, Content-Disposition, Offset"
-  response.headers["Access-Control-Allow-Headers"] = "Origin, X-Requested-With, Content-Type, Accept, Content-Disposition, Final-Length, file-type, file-path, file-checksum, Offset"
+  response.headers['Access-Control-Allow-Methods'] = 'HEAD,GET,PUT,POST,PATCH,DELETE'
+  response.headers['Access-Control-Allow-Origin'] = '*'
+  response.headers['Access-Control-Expose-Headers'] = 'Location, Range, Content-Disposition, Offset'
+  response.headers['Access-Control-Allow-Headers'] = 'Origin, X-Requested-With, Content-Type, Accept, Content-Disposition, Final-Length, file-type, file-path, file-checksum, Offset'
 end
 
+UPLOAD_FOLDER = 'uploads'
 
 # Handle OPTION-request (Check if we can upload files)
 # todo: add security
 options "/files/" do
-  if request.env["Access-Control-Request-Method"]=="POST"
+  if request.env['Access-Control-Request-Method']=='POST'
     status 200
-  elsif request.env["Access-Control-Request-Method"]=="PATCH"
+  elsif request.env['Access-Control-Request-Method']=='PATCH'
     status 404
   end
 end
@@ -27,17 +28,17 @@ end
 # todo: check avaliable drive space
 post "/files/" do
   unique_filename = "#{SecureRandom.hex}.tmp"
-  path = 'uploads/'+unique_filename
+  path = UPLOAD_FOLDER+'/'+unique_filename
 
-  File.write(path, "")
-  response.headers["Location"] = request.url+unique_filename
+  File.write(path, '')
+  response.headers['Location'] = request.url+unique_filename
   status 201
 end
 
 # Handle OPTIONS-request (Check if temporary file exists)
 options "/files/:name" do
-  file_name = params[:name]
-  path = 'uploads/'+file_name
+  temp_file_name = params[:name]
+  path = UPLOAD_FOLDER+'/'+temp_file_name
   if File.file?(path)
     status 200
   else
@@ -47,11 +48,11 @@ end
 
 # Handle PATCH-request (Receive and save the uploaded file in chunks)
 patch "/files/:name" do
-  file_name = params[:name]
-  path = 'uploads/'+file_name
+  temp_file_name = params[:name]
+  path = UPLOAD_FOLDER+'/'+temp_file_name
   offset = request.env['HTTP_OFFSET'].to_i
   begin
-    f = File.open(path, "r+b")
+    f = File.open(path, 'r+b')
     f.sync = true
     f.seek(offset) unless offset.nil?
     f.write(request.body.read)
@@ -68,21 +69,23 @@ end
 
 # Handle OPTIONS-request (Check if temporary file exists and return offset)
 head "/files/:name" do
-  file_name = params[:name]
-  path = 'uploads/'+file_name
+  temp_file_name = params[:name]
+  path = UPLOAD_FOLDER+'/'+temp_file_name
 
   if File.file?(path)
-    response.headers["Offset"] = File.size(path).to_s
+    response.headers['Offset'] = File.size(path).to_s
     status 200
   else
     status 404
   end
 end
 
+# Extend TUS Protocol
+
 # Handle OPTIONS-request (Check if file exists)
 head "/files/" do
   file_path = request.env['HTTP_FILE_PATH'].to_s
-  path = 'uploads/'+file_path
+  path = UPLOAD_FOLDER+'/'+file_path
 
   if File.file?(path)
     status 200
@@ -93,25 +96,27 @@ end
 
 # Handle PUT-request (Move and rename temporary file)
 put "/files/:name" do
-  file_name = params[:name]
+  temp_file_name = params[:name]
   file_path = request.env['HTTP_FILE_PATH'].to_s
 
   begin
-    create_path_folders(file_path, 'uploads', file_name)
+    move_file(file_path, temp_file_name)
   rescue
+    FileUtils.rm(UPLOAD_FOLDER+'/'+temp_file_name)
     halt 404
   end
-
+  response.headers['md5'] = Digest::MD5.file(UPLOAD_FOLDER+'/'+file_path).hexdigest
+  response.headers['Location'] = file_path
   status 200
 end
 
-def create_path_folders(path, upload_path, file_name)
+def move_file(path, temp_file_name)
   unless path.nil?
-    folders = path.split("/")
+    folders = path.split('/')
     folders.pop
     if folders.size>0
-      FileUtils.makedirs upload_path+'/'+folders.join('/')
+      FileUtils.makedirs UPLOAD_FOLDER+'/'+folders.join('/')
     end
-    FileUtils.mv(upload_path+'/'+file_name, upload_path+'/'+path)
+    FileUtils.mv(UPLOAD_FOLDER+'/'+temp_file_name, UPLOAD_FOLDER+'/'+path)
   end
 end
